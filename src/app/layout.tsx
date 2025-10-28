6afae76dd43418a30291fc1105af736b3cdabfe5
woghks2 <tbxkdls19@gmail.com>
2025-10-29 00:00:57 +0900

    feat: 목 데이터 API 호출하도록 변경
    - 하드코딩된 변수 제거
    - 목 데이터 호출

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,20 +11,22 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
-      <body
-        className="bg-background"
-        style={{margin: 0, padding: 0}}
-      >
+    <html lang="ko">
+      <head>
+
+        {/* Todo: tailwind나 globals.css쪽에서 적용안돼서 방법 찾으면 해결 예정입니다. */}
+        {/* Pretendard CDN — 100% 확실하게 적용됨 */}
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
+        />
+      </head>
+      <body className="font-[Pretendard] bg-background text-text">
         <div className="flex min-h-screen">
           <Sidebar />
-          <main className="flex-1 bg-background">
-            {children}
-          </main>
+          <main className="flex-1 bg-background">{children}</main>
         </div>
       </body>
     </html>
