9708d86050060d05302814aa23c8253b821b5d03
woghks2 <tbxkdls19@gmail.com>
2025-10-28 13:39:29 +0900

    chore: 아이콘 크기 변경
    - 24x24 크기로 아이콘 크기 변경

--- src/components/icon/MagnifyingGlass.tsx
+++ src/components/icon/MagnifyingGlass.tsx
@@ -2,8 +2,8 @@
 
 export default function MagnifyingGlass() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
-  <path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" />
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
     </svg>
   )
 }
