2a2a4e4f1f0e8d1e762356b246ff99c03589a38a
woghks2 <tbxkdls19@gmail.com>
2025-10-28 15:35:04 +0900

    comment: 메인 page.tsx, HeroSection.tsx 주석 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@
 
 import HeroSection from "@/components/section/HeroSection";
 
+
+/**
+ * Home component
+ * @description Home component is a home component that displays the home page content
+ * @returns {React.ReactNode}
+ */
 export default function Home() {
   return <HeroSection />;
 }
