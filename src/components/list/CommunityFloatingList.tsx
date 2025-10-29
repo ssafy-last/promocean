5b8a64cee93e51052d2da44b2499188ce6bb95e5
woghks2 <tbxkdls19@gmail.com>
2025-10-29 15:09:22 +0900

    feat: 우측 플로팅 인기글 섹션 추가 [S13P31A208-115]
    - 우측 인기글 섹션 추가

--- src/components/list/CommunityFloatingList.tsx
+++ src/components/list/CommunityFloatingList.tsx
@@ -1,13 +1,21 @@
 // frontend/src/components/list/CommunityFloatingList.tsx
 
 import CommunityFloatingItem from '@/components/item/CommunityFloatingItem'
+import { CommunityFloatingItemProps } from '@/types/item'
 
-export default function CommunityFloatingList() {
+interface CommunityFloatingListProps {
+  popularPosts: CommunityFloatingItemProps[]
+}
+
+export default function CommunityFloatingList({ popularPosts }: CommunityFloatingListProps) {
   return (
-    <div>
-      <h1>Community Floating List</h1>
-      <CommunityFloatingItem />
-      <CommunityFloatingItem />
+    <div className="space-y-0">
+      {popularPosts.map((item) => (
+        <CommunityFloatingItem
+          key={item.id}
+          {...item}
+        />
+      ))}
     </div>
   )
 }
