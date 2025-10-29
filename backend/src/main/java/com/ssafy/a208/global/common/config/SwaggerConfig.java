8b27ab33df6185ee9ce5693ac3815620741294ca
이준희 <dlwnsfml@naver.com>
2025-10-29 19:11:05 +0900

    setting: 스웨거 설정 [S13P31A208-101]

---
+++ backend/src/main/java/com/ssafy/a208/global/common/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.ssafy.a208.global.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String SCHEMA_NAME = "Authorization";
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(apiInfo())
+                .components(appAuthorization())
+                .addSecurityItem(security());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("PROMOCEAN")
+                .description("RestAPI 잔뜩 쌓아두는 Swagger")
+                .version("1.0.0");
+    }
+
+    private Components appAuthorization() {
+        return new Components()
+                .addSecuritySchemes(SCHEMA_NAME, new SecurityScheme()
+                        .name(SCHEMA_NAME)
+                        .type(Type.APIKEY)
+                        .in(In.HEADER));
+    }
+
+    private SecurityRequirement security() {
+        return new SecurityRequirement().addList(SCHEMA_NAME);
+    }
+
+}
