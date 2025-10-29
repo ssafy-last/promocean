0340764d83fb2da49f753e5de7cbda522431bb27
woghks2 <tbxkdls19@gmail.com>
2025-10-29 16:08:54 +0900

    feat: 페이지네이션 컴포넌트 및 아이콘 추가 [S13P31A208-115]

--- src/components/layout/CommunityFooter.tsx
+++ src/components/layout/CommunityFooter.tsx
@@ -2,14 +2,13 @@
 
 export default function CommunityFooter() {
   return (
-    <div>
+    <div className="flex justify-center items-center py-10">
       <div className="join">
-      <button className="join-item btn">1</button>
-      <button className="join-item btn btn-active">2</button>
-      <button className="join-item btn">3</button>
-      <button className="join-item btn">4</button>
-      <button className="join-item btn">5</button>
+        <button className="join-item btn">1</button>
+        <button className="join-item btn btn-active">2</button>
+        <button className="join-item btn">3</button>
+        <button className="join-item btn">4</button>
       </div>
     </div>
-  )
+  );
 }
