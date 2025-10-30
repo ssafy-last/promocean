1d6e9b279094dc40443274fad2f9ad3549c79dc1
woghks2 <tbxkdls19@gmail.com>
2025-10-30 09:14:04 +0900

    feat: 로그인 관련 페이지 추가 [S13P31A208-113]
    - 로그인 탭 추가
    - 로그인 폼 추가
    - 회원가입 폼 추가
    - 회원관리 스토어 추가

---
+++ src/components/form/SignInForm.tsx
@@ -0,0 +1,9 @@
+// frontend/src/components/form/SignInForm.tsx
+
+export default function SignInForm() {
+  return (
+    <div>
+      <h1>Sign In</h1>
+    </div>
+  );
+}
---
+++ src/components/form/SignUpForm.tsx
@@ -0,0 +1,9 @@
+// frontend/src/components/form/SignUpForm.tsx
+
+export default function SignUpForm() {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+    </div>
+  );
+}
