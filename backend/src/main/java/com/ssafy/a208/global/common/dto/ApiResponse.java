fae4a807a6332a241c702ec1d65cdbf18cf2d248
이준희 <dlwnsfml@naver.com>
2025-10-30 09:28:59 +0900

    setting: 공통 dto 설정 [S13P31A208-98]

---
+++ backend/src/main/java/com/ssafy/a208/global/common/dto/ApiResponse.java
@@ -0,0 +1,52 @@
+package com.ssafy.a208.global.common.dto;
+
+import com.ssafy.a208.global.common.exception.ApiException;
+import lombok.Builder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Builder
+public record ApiResponse<T>(
+        String message,
+        T data
+) {
+
+    public static <T> ResponseEntity<ApiResponse<T>> of(HttpStatus status) {
+        return ResponseEntity.status(status)
+                .body(ApiResponse.<T>builder().build());
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> of(HttpStatus status, String message) {
+        return ResponseEntity.status(status)
+                .body(ApiResponse.<T>builder().message(message).build());
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> of(HttpStatus status, T data) {
+        return ResponseEntity.status(status)
+                .body(ApiResponse.<T>builder().data(data).build());
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> of(HttpStatus status, String message, T data) {
+        return ResponseEntity.status(status)
+                .body(ApiResponse.<T>builder().message(message).data(data).build());
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> failedOf(ApiException e) {
+        return ResponseEntity.status(e.status)
+                .body(ApiResponse.<T>builder().message(e.getMessage()).build());
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> ok() {
+        return ApiResponse.of(HttpStatus.OK);
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> ok(T data) {
+        return ApiResponse.of(HttpStatus.OK, data);
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> create(T data) {
+        return ApiResponse.of(HttpStatus.CREATED, data);
+    }
+
+}
+
