b687ff714160ba22a51922176a149d14599c6134
Jung TaeSeung <mike415415@naver.com>
2025-11-01 04:28:50 +0900

    feat : 아카이브 섹션 컴포넌트 추가 [S13P31A208-125]

---
+++ src/components/section/MySpaceArchiveFilterSection.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+// frontend/src/components/section/CommunityBoardFilterSection.tsx
+
+import ComboBox from "@/components/filter/ComboBox";
+import SearchBar from "@/components/filter/SearchBar";
+import { useRouter } from "next/navigation";
+
+/**
+ * CommunityBoardFilterSection component
+ * @description CommunityBoardFilterSection component is a community board filter section component that displays the community board filter section content
+ * @returns {React.ReactNode}
+ */
+export default function MySpaceArchiveFilterSection() {
+
+  const router = useRouter();
+  const handleArchiveSearch = () => {
+    console.log("카테고리 검색 클릭");
+  }
+
+  return (
+    <div className="flex flex-row items-center justify-end gap-3 w-full px-4 py-4 bg-gray-50 border-gray-200">
+      <ComboBox />
+      <SearchBar />
+
+      {/* Todo: 게시글 작성 컴포넌트 분리 */}
+      <button
+        type="button"
+        className="px-4 py-2 rounded-md bg-primary text-white font-medium hover:brightness-110 active:brightness-95 transition-colors"
+        onClick={handleArchiveSearch}
+      >
+        카테고리 검색
+      </button>
+    </div>
+  );
+}
