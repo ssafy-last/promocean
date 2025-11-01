c21c2c8dee0f6f5b653107e560925a28b0ac1ccb
Jung TaeSeung <mike415415@naver.com>
2025-11-02 00:14:38 +0900

    feat : 스크랩 페이지 생성  [S13P31A208-125]

---
+++ src/app/my-space/scrap/page.tsx
@@ -0,0 +1,14 @@
+// frontend/src/app/my-space/scrap/[id]/page.tsx
+export default function MySpaceScrapPage() {
+return (
+
+    <div className="min-h-screen bg-gray-50">
+
+      <div className="flex justify-start max-w-6xl px-4 py-3 text-[40px] font-bold text-gray-900">
+        <h1 className="text-[40px] font-bold text-gray-900">스크랩</h1>
+      </div>
+
+    </div>
+
+  );
+}
