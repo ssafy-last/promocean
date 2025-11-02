e33efd58abe0d8e3654718a9329e4485a579037a
Jung TaeSeung <mike415415@naver.com>
2025-11-02 22:51:07 +0900

    comment : 쓸모없는 주석 삭제  [S13P31A208-125]

--- src/app/my-space/page.tsx
+++ src/app/my-space/page.tsx
@@ -26,22 +26,9 @@ export default async function MySpacePage() {
   const MockPinnedSpaceArchiveItemList = mySpaceData.pinned;
   const MockFolderSpaceArchiveItemList = mySpaceData.normal;
 
-  // const MockPinnedSpaceArchiveItemList = [
-  //   {title: "AI 챗봇", bgColor: "bg-blue-200"},
-  //   {title: "이미지 생성", bgColor: "bg-[#0949ad]"},
-  //   {title: "스파팸", bgColor: "bg-green-200"},
-  //   {title: "카드 이름", bgColor: "bg-red-200"},
-  //   {title: "바보", bgColor: "bg-purple-200"},
-  // ]
-
-
-  // const MockFolderSpaceArchiveItemList = [
-  //   {title: "문서 작성", bgColor: "bg-yellow-200"},
-  //   {title: "코드 생성", bgColor: "bg-pink-200"},
-  //   {title: "데이터 분석", bgColor: "bg-teal-200"},
-  //   {title: "프로젝트 관리", bgColor: "bg-indigo-200"},
-  //   {title: "학습 도우미", bgColor: "bg-lime-200"},
-  // ]
+  const testFunction = ()=>{
+    console.log("click");
+  }
 
   return (
     <div className="min-h-screen bg-gray-50">
