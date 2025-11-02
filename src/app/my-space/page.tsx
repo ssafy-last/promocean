6b55f5370e934568157a10d809053f026286eb6c
Jung TaeSeung <mike415415@naver.com>
2025-11-02 22:41:20 +0900

    feat : json 호출 및 아이템 리스트에 적용  [S13P31A208-125]

--- src/app/my-space/page.tsx
+++ src/app/my-space/page.tsx
@@ -4,28 +4,44 @@ import SpaceArchiveItem from "@/components/item/SpaceArchiveItem";
 import MySpaceHeader from "@/components/layout/MySpaceHeader";
 import SpaceCardHeader from "@/components/layout/SpaceCardHeader";
 import SpaceArchiveList from "@/components/list/SpaceArchiveList";
-import CommunityBoardFilterSection from "@/components/section/CommunityBoardFilterSection";
 import MySpaceArchiveFilterSection from "@/components/section/MySpaceArchiveFilterSection";
 
+export interface SpaceArchiveData{
+  title:string,
+  bgColor :string
+}
+
 // frontend/src/app/my-space/page.tsx
-export default function MySpacePage() {
+export default async function MySpacePage() {
   
-  const MockPinnedSpaceArchiveItemList = [
-    {title: "AI 챗봇", bgColor: "bg-blue-200"},
-    {title: "이미지 생성", bgColor: "bg-[#0949ad]"},
-    {title: "스파팸", bgColor: "bg-green-200"},
-    {title: "카드 이름", bgColor: "bg-red-200"},
-    {title: "바보", bgColor: "bg-purple-200"},
-  ]
+
+  const mySpaceArchiveRes = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"}/mock/MySpaceArchiveData.json`,
+    { cache: "no-store" }
+  );
+
+  const mySpaceData = await mySpaceArchiveRes.json();
+  console.log("data ",mySpaceData)
+
+  const MockPinnedSpaceArchiveItemList = mySpaceData.pinned;
+  const MockFolderSpaceArchiveItemList = mySpaceData.normal;
+
+  // const MockPinnedSpaceArchiveItemList = [
+  //   {title: "AI 챗봇", bgColor: "bg-blue-200"},
+  //   {title: "이미지 생성", bgColor: "bg-[#0949ad]"},
+  //   {title: "스파팸", bgColor: "bg-green-200"},
+  //   {title: "카드 이름", bgColor: "bg-red-200"},
+  //   {title: "바보", bgColor: "bg-purple-200"},
+  // ]
 
 
-  const MockFolderSpaceArchiveItemList = [
-    {title: "문서 작성", bgColor: "bg-yellow-200"},
-    {title: "코드 생성", bgColor: "bg-pink-200"},
-    {title: "데이터 분석", bgColor: "bg-teal-200"},
-    {title: "프로젝트 관리", bgColor: "bg-indigo-200"},
-    {title: "학습 도우미", bgColor: "bg-lime-200"},
-  ]
+  // const MockFolderSpaceArchiveItemList = [
+  //   {title: "문서 작성", bgColor: "bg-yellow-200"},
+  //   {title: "코드 생성", bgColor: "bg-pink-200"},
+  //   {title: "데이터 분석", bgColor: "bg-teal-200"},
+  //   {title: "프로젝트 관리", bgColor: "bg-indigo-200"},
+  //   {title: "학습 도우미", bgColor: "bg-lime-200"},
+  // ]
 
   return (
     <div className="min-h-screen bg-gray-50">
