bb9f133a0904ad5b1a103d9c3fe12913edda4449
Jung TaeSeung <mike415415@naver.com>
2025-11-02 22:49:57 +0900

    feat : 스페이스 탭바 텍스트 크기 조정  [S13P31A208-125]

--- src/components/filter/MySpaceTabs.tsx
+++ src/components/filter/MySpaceTabs.tsx
@@ -25,7 +25,7 @@ export default function MySpaceTabs() {
             <li key={cat.name}>
               <Link
                 href={href}
-                className={`inline-block py-3 text-sm font-medium transition-colors ${
+                className={`inline-block py-3 text-lg font-medium transition-colors ${
                   isActive
                     ? "text-primary border-b-2 border-primary"
                     : "text-gray-600 hover:text-primary"
