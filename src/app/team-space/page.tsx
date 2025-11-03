9b6d886f327d72dd8c69dd1b2f731b04c7ea0ebe
Jeong TaeSeung <mike415415@naver.com>
2025-11-03 15:48:47 +0900

    feat: team space mock 객체 생성  [S13P31A208-158]

--- src/app/team-space/page.tsx
+++ src/app/team-space/page.tsx
@@ -5,40 +5,14 @@ import SpaceHeader from "@/components/layout/SpaceHeader";
 import TeamSpaceTeamChoiceList from "@/components/list/TeamSpaceTeamChoiceLlist";
 
 // frontend/src/app/team-space/page.tsx
-export default function TeamSpacePage() {
-  const teamSpcaeTeamChoiceList : TeamSpaceTeamChoiceItemProps[] =[
-    //임시 데이터
-    {
-      image: "/images/team-space/team1.png",
-      title: "팀 스페이스 1",
-      description: "팀 스페이스 1 설명입니다."
-    },
-    {
-      image: "/images/team-space/team2.png",
-      title: "팀 스페이스 2",
-      description: "팀 스페이스 2 설명입니다."
-    },
-    {
-      image: "/images/team-space/team3.png",
-      title: "팀 스페이스 3",
-      description: "팀 스페이스 3 설명입니다."
-    },
-    {
-      image: "/images/team-space/team4.png",
-      title: "팀 스페이스 4",
-      description: "팀 스페이스 4 설명입니다."
-    },
-    {
-      image: "/images/team-space/team1.png",
-      title: "팀 스페이스 5",
-      description: "팀 스페이스 5 설명입니다."
-    },
-    {
-      image: "/images/team-space/team2.png",
-      title: "팀 스페이스 6",
-      description: "팀 스페이스 6 설명입니다."
-    },
-    ];
+export default async function TeamSpacePage() {
+  
+  const teamSpaceArchiveRes = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"}/mock/TeamSpaceData.json`,
+    { cache: "no-store" }
+  );
+
+  const teamSpcaeTeamChoiceList : TeamSpaceTeamChoiceItemProps[] = await teamSpaceArchiveRes.json();
   
 
   return (
