8b5094fb51c40e93a74866e8df4265c95563ab83
leedy903 <leedy903@naver.com>
2025-11-03 13:10:46 +0900

    refactor: 스페이스 접근 권한 예외 처리 SpaceAccessDeniedException 하나로 처리하도록 변경 [S13P31A208-127]

--- backend/src/main/java/com/ssafy/a208/domain/space/exception/space/SpaceAccessDeniedException.java
+++ backend/src/main/java/com/ssafy/a208/domain/space/exception/space/SpaceAccessDeniedException.java
@@ -5,9 +5,7 @@ import org.springframework.http.HttpStatus;
 
 public class SpaceAccessDeniedException extends ApiException {
 
-    private static final String MESSAGE = "스페이스 접근 권한이 없는 회원입니다.";
-
-    public SpaceAccessDeniedException() {
-        super(HttpStatus.FORBIDDEN, MESSAGE);
+    public SpaceAccessDeniedException(String message) {
+        super(HttpStatus.FORBIDDEN, message);
     }
 }
