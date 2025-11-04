9fd838fef611231bfbcaae6be2c01ece3bd8e5bf
woghks2 <tbxkdls19@gmail.com>
2025-11-04 15:22:06 +0900

    feat: 커뮤니티 글 상세 댓글 컴포넌트 추가[S13P31A208-180]
    - 댓글 입력 폼 추가
    - 사용자 댓글 추가
    - 목 데이터 추가

--- src/components/section/CommunityLikeShareSection.tsx
+++ src/components/section/CommunityLikeShareSection.tsx
@@ -2,20 +2,35 @@
 
 import Heart from "@/components/icon/Heart";
 import Share from "@/components/icon/Share";
+import ChatBubbleBottomCenterText from "@/components/icon/ChatBubbleBottomCenterText";
 
 /**
  * CommunityLikeShareSection component
  * @description CommunityLikeShareSection component is a community like share section component that displays the community like share section content
  * @returns {React.ReactNode}
  */
-export default function CommunityLikeShareSection( { likeCount }: { likeCount: number } ) {
+export default function CommunityLikeShareSection( { likeCount , commentCount }: { likeCount: number, commentCount: number } ) {
   return (
-    <div className="flex flex-row items-center gap-2">
-      <div className="flex items-center gap-1">
-        <Heart />
-        <span className="text-sm">{likeCount}</span>
+    <div className="flex flex-row items-center justify-between">
+      {/* 왼쪽: 좋아요, 댓글 */}
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-1">
+          <Heart />
+          <span className="text-sm">{likeCount}</span>
+        </div>
+        <div className="flex items-center gap-1">
+          <ChatBubbleBottomCenterText />
+          <span className="text-sm">{commentCount}</span>
+        </div>
       </div>
-      <Share />
+      
+      {/* 오른쪽: 공유하기 */}
+      <div>
+        <div className="flex items-center gap-1">
+          <Share />
+          <span className="text-sm">공유</span>
+        </div>
+      </div>
     </div>
   )
 }
