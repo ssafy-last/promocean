6ac58b3103ceb0c8a523b0aae4fe87a755147a41
woghks2 <tbxkdls19@gmail.com>
2025-11-04 10:17:43 +0900

    feat: 커뮤니티 상세 게시글 페이지 컴포넌트 추가 [feature/community/S13P31A208-180]
    - 상세 게시글 컴포넌트 추가
    - 해시태그 컴포넌트 추가
    - 좋아요 및 스크랩 컴포넌트 추가
    - 댓글 컴포넌트 추가
    - 인기글 플로팅 컴포넌트 추가

---
+++ src/components/section/CommunityCommentSection.tsx
@@ -0,0 +1,14 @@
+// frontend/src/components/section/CommunityCommentSection.tsx
+
+/**
+ * CommunityCommentSection component
+ * @description CommunityCommentSection component is a community comment section component that displays the community comment section content
+ * @returns {React.ReactNode}
+ */
+export default function CommunityCommentSection() {
+  return (
+    <div>
+      <h1>CommunityCommentSection</h1>
+    </div>
+  )
+}
