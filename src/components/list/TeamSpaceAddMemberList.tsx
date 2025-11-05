f63535bbd449fae66b290dd2a8a6ffc133480716
Jeong TaeSeung <mike415415@naver.com>
2025-11-05 12:21:41 +0900

    fix : 검색 리스트 창 absolute 로 수정 (요소 공간 차지 방지)  [S13P31A208-158]

--- src/components/list/TeamSpaceAddMemberList.tsx
+++ src/components/list/TeamSpaceAddMemberList.tsx
@@ -22,7 +22,7 @@ export default function TeamSpaceAddMemberList({ searchSpaceMemberListState, sel
     };
 
     return(
-        <div className ="overflow-y-scroll w-[600px] h-[300px] border border-t-0 border-gray-300 rounded-[10px] p-2.5 gap-2.5">
+        <div className ="absolute bg-white overflow-y-scroll w-full h-[300px] border border-t-0 border-gray-300 rounded-[10px] p-2.5 gap-2.5">
             {searchSpaceMemberListState.map((member) => (
                 <SpaceAddMemberItem key={member.email} name={member.name} email={member.email} handleMemberClick={handleMemberClick} />
             ))}
