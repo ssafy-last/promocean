bfe84c9ecade5cc25f908c31b7694132e6aace33
Jeong TaeSeung <mike415415@naver.com>
2025-11-05 10:20:38 +0900

    fix : SpaceAddMemberItem 버튼 태그로 수정  [S13P31A208-159]

--- src/components/item/SpaceAddMemberItem.tsx
+++ src/components/item/SpaceAddMemberItem.tsx
@@ -11,13 +11,13 @@ export interface SpaceAddMemberItemProps {
 export default function SpaceAddMemberItem({name, email}: SpaceAddMemberItemProps){
 
     return(
-        <div className ="flex flex-row px-4 py-1 gap-3 items-center hover:bg-current/5 active:bg-current/10">
+        <button className ="flex flex-row w-full px-4 py-1 gap-3 items-center hover:bg-current/5 active:bg-current/10">
             <div className = "rounded-full bg-gray-200 border w-10 h-10"></div>
-            <div>
+            <div className = "text-left">
                 <span className="font-medium text-lg">{name}</span>
                 <p className="text-gray-400">{email}</p>
             </div>
-        </div>
+        </button>
     )
 
 }
