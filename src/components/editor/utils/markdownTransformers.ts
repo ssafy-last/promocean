fd5923cda052357d34936d0624bce0e67d9be029
Jeong TaeSeung <mike415415@naver.com>
2025-11-06 12:24:04 +0900

    feat : toolbar 컴포넌트 추가  [S13P31A208-217]

---
+++ src/components/editor/utils/markdownTransformers.ts
@@ -0,0 +1,100 @@
+import {
+  CHECK_LIST,
+  ELEMENT_TRANSFORMERS,
+  TEXT_FORMAT_TRANSFORMERS,
+  TEXT_MATCH_TRANSFORMERS,
+  ElementTransformer,
+  TextMatchTransformer,
+  LINK,
+} from '@lexical/markdown';
+import { CodeNode, $createCodeNode } from '@lexical/code';
+import {
+  HorizontalRuleNode,
+  $createHorizontalRuleNode,
+} from '@lexical/react/LexicalHorizontalRuleNode';
+import { $createParagraphNode, $createTextNode, TextNode } from 'lexical';
+
+const HR: ElementTransformer = {
+  dependencies: [HorizontalRuleNode],
+  export: (node) => {
+    return node.getType() === 'horizontalrule' ? '***' : null;
+  },
+  regExp: /^(---|\*\*\*|___)\s?$/,
+  replace: (parentNode, _1, _2, isImport) => {
+    const line = $createHorizontalRuleNode();
+
+    if (isImport || parentNode.getNextSibling() != null) {
+      parentNode.replace(line);
+    } else {
+      parentNode.insertBefore(line);
+    }
+
+    // 구분선 뒤에 새 문단 추가하여 커서 위치 설정
+    const paragraph = $createParagraphNode();
+    line.insertAfter(paragraph);
+    paragraph.select();
+  },
+  type: 'element',
+};
+
+const CODE_BLOCK: ElementTransformer = {
+  dependencies: [CodeNode],
+  export: (node) => {
+    if (node.getType() !== 'code') {
+      return null;
+    }
+    const textContent = node.getTextContent();
+    return '```\n' + textContent + '\n```';
+  },
+  regExp: /^```(\w+)?\s/,
+  replace: (parentNode, _1, match) => {
+    const codeNode = $createCodeNode(match ? match[1] : undefined);
+    parentNode.replace(codeNode);
+    // 코드 블럭 내부로 포커스 이동
+    codeNode.select(0, 0);
+  },
+  type: 'element',
+};
+
+// 해시태그 transformer
+const HASHTAG: TextMatchTransformer = {
+  dependencies: [TextNode],
+  export: (node) => {
+    if (!node.hasFormat('code')) {
+      return null;
+    }
+    const text = node.getTextContent();
+    if (text.startsWith('#')) {
+      return text;
+    }
+    return null;
+  },
+  importRegExp: /#[\w가-힣]+/,
+  regExp: /#[\w가-힣]+$/,
+  replace: (textNode, match) => {
+    const hashtagText = match[0];
+    const hashtagNode = $createTextNode(hashtagText);
+    hashtagNode.setFormat('code'); // 'code' format을 사용하여 해시태그 스타일 적용
+    textNode.replace(hashtagNode);
+  },
+  trigger: ' ',
+  type: 'text-match',
+};
+
+// 링크 transformer를 제외한 transformers
+const filteredElementTransformers = ELEMENT_TRANSFORMERS.filter(
+  (transformer) => transformer !== LINK
+);
+const filteredTextMatchTransformers = TEXT_MATCH_TRANSFORMERS.filter(
+  (transformer) => transformer !== LINK
+);
+
+export const CUSTOM_TRANSFORMERS = [
+  ...filteredElementTransformers,
+  ...TEXT_FORMAT_TRANSFORMERS,
+  ...filteredTextMatchTransformers,
+  HASHTAG,
+  HR,
+  CODE_BLOCK,
+  CHECK_LIST,
+];
