9702d77871c5be44c9af381121e6826892850a15
woghks2 <tbxkdls19@gmail.com>
2025-11-06 17:08:13 +0900

    feat: 컨테스트 API 리팩토링 [S13P31A208-214]
    - api fetcher 리팩토링
    - 토큰 관련 코드 리팩토링
    - contrest 페이지 진입 시 contest.ts 호출 후 사용

---
+++ src/api/contest.ts
@@ -0,0 +1,48 @@
+// frontend/src/api/contest.ts
+
+import { apiFetch } from "@/api/fetcher";
+import {
+  ContestPostItemProps,
+  LeaderboardItemProps,
+  ContestInfoItemProps,
+  ContestNoticeItemProps,
+  ContestSubmissionItemProps,
+} from "@/types/itemType";
+
+/**
+ * ContestAPI
+ * @description ContestAPI is a contest API that fetches contest post page data
+ * @returns {Promise<{ contestPostData: ContestPostItemProps, leaderboardList: LeaderboardItemProps[], contestInfoData: ContestInfoItemProps[], contestInfoTitles: string[], contestNoticeList: ContestNoticeItemProps[], contestSubmissionList: ContestSubmissionItemProps[] }>}
+ */
+export const ContestAPI = {
+
+  /**
+   * 대회 상세 페이지 데이터 조회
+   * @description 대회 상세 페이지 데이터를 조회하는 API입니다. // Todo : 실제 API와 연동하기
+   * @returns {Promise<{ contestPostData: ContestPostItemProps, leaderboardList: LeaderboardItemProps[], contestInfoData: ContestInfoItemProps[], contestInfoTitles: string[], contestNoticeList: ContestNoticeItemProps[], contestSubmissionList: ContestSubmissionItemProps[] }>}
+   */
+  async getContestPostPageData() {
+    const [
+      contestPost,
+      leaderboard,
+      contestInfo,
+      contestNotice,
+      contestSubmission,
+    ] = await Promise.all([
+      apiFetch<ContestPostItemProps>("/mock/ContestPostItem.json"),
+      apiFetch<{ Leaderboard: LeaderboardItemProps[] }>("/mock/LeaderboardItem.json"),
+      apiFetch<{ items: ContestInfoItemProps[] }>("/mock/ContestInfoItem.json"),
+      apiFetch<ContestNoticeItemProps[]>("/mock/ContestNoticeData.json"),
+      apiFetch<ContestSubmissionItemProps[]>("/mock/ContestSubmissionData.json"),
+    ]);
+
+    return {
+      contestPostData: contestPost,
+      leaderboardList: leaderboard.Leaderboard || [],
+      contestInfoData: contestInfo.items || [],
+      contestInfoTitles: ["대회 정보", "상금 유형", "참여 통계", "해시태그"],
+      contestNoticeList: contestNotice || [],
+      contestSubmissionList: contestSubmission || [],
+    };
+  },
+};
