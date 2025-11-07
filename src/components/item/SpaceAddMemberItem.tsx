cda34d833e385161e18ba6d1f0f9afa22a5fd897
Jeong TaeSeung <mike415415@naver.com>
2025-11-07 17:21:11 +0900

    feat: space add member item 에 우측 제거 버튼 추가 [S13P31A208-217]

--- src/components/item/SpaceAddMemberItem.tsx
+++ src/components/item/SpaceAddMemberItem.tsx
@@ -3,25 +3,35 @@
 export interface SpaceAddMemberItemProps {
     name : string,
     email : string
+    isMinusButton? :boolean;
     handleMemberClick?: (memberName: string) => void;
 }
 
 
 
-export default function SpaceAddMemberItem({name, email, handleMemberClick}: SpaceAddMemberItemProps){
+export default function SpaceAddMemberItem({name, email, handleMemberClick, isMinusButton=true}: SpaceAddMemberItemProps){
 
     return(
         <button 
         type="button"
-        className ="flex flex-row w-full px-4 py-1 gap-3 items-center hover:bg-current/5 active:bg-current/10
+        className ="flex flex-row w-full justify-between px-4 py-1 gap-3 items-center hover:bg-current/5 active:bg-current/10
         rounded-md"
         onClick={()=>{handleMemberClick?.(name)}}
         >
+            <div className="flex gap-3 items-center justify-center-safe">
             <div className = "rounded-full bg-gray-200 border w-10 h-10"></div>
             <div className = "text-left">
                 <span className="font-medium text">{name}</span>
                 <p className="text-sm text-gray-400">{email}</p>
             </div>
+            </div>
+
+            {isMinusButton && ( 
+            <div className = "flex flex-col">
+            <button className="flex bg-primary rounded-full size-5 text-white justify-center items-center hover:bg-primary/70 active:bg-primary/100"
+            onClick={(e)=>{e.preventDefault()}}>-</button>
+            </div>
+            )}
         </button>
     )
 
