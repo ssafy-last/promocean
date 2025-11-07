6de9e5cad7993a7ffbb281ab55ca435b3027192a
Jeong TaeSeung <mike415415@naver.com>
2025-11-07 16:33:57 +0900

    chore : add member item 디자인 수정 [S13P31A208-217]

--- src/components/item/SpaceAddMemberItem.tsx
+++ src/components/item/SpaceAddMemberItem.tsx
@@ -13,13 +13,14 @@ export default function SpaceAddMemberItem({name, email, handleMemberClick}: Spa
     return(
         <button 
         type="button"
-        className ="flex flex-row w-full px-4 py-1 gap-3 items-center hover:bg-current/5 active:bg-current/10"
+        className ="flex flex-row w-full px-4 py-1 gap-3 items-center hover:bg-current/5 active:bg-current/10
+        rounded-md"
         onClick={()=>{handleMemberClick?.(name)}}
         >
             <div className = "rounded-full bg-gray-200 border w-10 h-10"></div>
             <div className = "text-left">
-                <span className="font-medium text-lg">{name}</span>
-                <p className="text-gray-400">{email}</p>
+                <span className="font-medium text">{name}</span>
+                <p className="text-sm text-gray-400">{email}</p>
             </div>
         </button>
     )
