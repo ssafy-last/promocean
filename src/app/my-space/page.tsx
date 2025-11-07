0566567080c0a394fd7211207de7a3272359503e
Jeong TaeSeung <mike415415@naver.com>
2025-11-07 10:25:44 +0900

    refactor : SpaceArchive data Response 인터페이스 생성  [S13P31A208-217]

--- src/app/my-space/page.tsx
+++ src/app/my-space/page.tsx
@@ -15,6 +15,20 @@ export interface SpaceArchiveData {
   isPinned: boolean;
 }
 
+/**
+ * MySpaceArchiveDataResponse 인터페이스
+ * @property {SpaceArchiveData[]} pinned - Pinned 항목들의 배열
+ * @property {SpaceArchiveData[]} normal - Normal 항목들의 배열
+ * 
+ * (이런 식으로 dto를 묶어서 관리하는 게 항상 더 좋아요)
+ * */
+export interface MySpaceArchiveDataResponse{
+  pinned: SpaceArchiveData[];
+  normal: SpaceArchiveData[];
+}
+
+
+
 export default function MySpacePage() {
   const [archiveItemListState, setArchiveItemListState] = useState<SpaceArchiveData[]>([]);
   const [pinnedItemListState, setPinnedItemListState] = useState<SpaceArchiveData[]>([]);
@@ -23,12 +37,14 @@ export default function MySpacePage() {
   useEffect(() => {
     const fetchData = async () => {
       try {
+        
+        //TODO : 백엔드 API 연결 후 수정 필요
         const mySpaceArchiveRes = await fetch(
           `${process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"}/mock/MySpaceArchiveData.json`,
           { cache: "no-store" }
         );
 
-        const mySpaceData = await mySpaceArchiveRes.json();
+        const mySpaceData = await mySpaceArchiveRes.json() as MySpaceArchiveDataResponse;
         console.log("data ", mySpaceData);
 
         setPinnedItemListState(mySpaceData.pinned || []);
