478179e1ad2f8555f7cde71c7f580fa88c84dd0c
Jeong TaeSeung <mike415415@naver.com>
2025-11-07 11:02:28 +0900

    feat : 스페이스 게시판 헤더 모든 하위 컴포넌트에 적용  [S13P31A208-217]

--- src/app/my-space/scrap/page.tsx
+++ src/app/my-space/scrap/page.tsx
@@ -1,4 +1,5 @@
 import { SpaceScrapBoardItemProps } from "@/components/item/SpaceScrapBoardItem";
+import { SpaceBoardHeader } from "@/components/layout/SpaceBoardHeader";
 import SpaceScrapBoardList from "@/components/list/SpaceScrapBoardList";
 import MySpaceArchiveFilterSection from "@/components/section/MySpaceArchiveFilterSection"
 
@@ -15,9 +16,7 @@ return (
 
     <div className="min-h-screen bg-gray-50">
   
-      <div className="flex justify-start max-w-6xl px-4 py-3 text-[40px] font-bold text-gray-900">
-        <h1 className="text-[40px] font-bold text-gray-900">스크랩</h1>
-      </div>
+      <SpaceBoardHeader titleName="스크랩" description="스크랩한 글을 모아볼 수 있습니다." />
       <MySpaceArchiveFilterSection buttonMode="search"/>
 
       <SpaceScrapBoardList itemList={spaceScrapBoardList}/>
