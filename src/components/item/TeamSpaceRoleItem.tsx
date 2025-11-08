5f097c5c380fc92dd194b35b41b90a47d1365952
Jung TaeSeung <mike415415@naver.com>
2025-11-09 00:16:55 +0900

    refactor: 팀 스페이스 권한을 enum 타입으로 변경  [S13P31A208-219]

--- src/components/item/TeamSpaceRoleItem.tsx
+++ src/components/item/TeamSpaceRoleItem.tsx
@@ -1,5 +1,11 @@
 
 
+export enum TeamSpaceRole {
+    READ_ONLY = 10,
+    EDIT = 20,
+    OWNER = 30
+}
+
 export interface TeamSpaceRoleItemProps {
     member: string;
     index: number;
@@ -14,9 +20,9 @@ export default function TeamSpaceRoleItem({ member, index }: TeamSpaceRoleItemPr
                 <select name="" id="" className ="hover:bg-gray-200 p-1 border-gray-200
                 outline-none
                 rounded-md">
-                    <option value="10">읽기 허용</option>
-                    <option value="20">편집 허용</option>
-                    <option value="30">소유자</option>
+                    <option value={TeamSpaceRole.READ_ONLY}>읽기 허용</option>
+                    <option value={TeamSpaceRole.EDIT}>편집 허용</option>
+                    <option value={TeamSpaceRole.OWNER}>소유자</option>
                 </select>
             </li>
     )
