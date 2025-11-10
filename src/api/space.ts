70d75678000d01918f079606e8bc5522e6a48a06
Jeong TaeSeung <mike415415@naver.com>
2025-11-10 18:37:00 +0900

    feat : space 폴더 추가 api + 조회 api 작업 완료 [S13P31A208-253]

--- src/api/space.ts
+++ src/api/space.ts
@@ -1,5 +1,16 @@
-import { MySpaceArchiveDataResponse } from "@/app/my-space/page";
+import {  SpaceArchiveData } from "@/app/my-space/page";
 import { apiFetch } from "./fetcher";
+
+
+export interface ApiResponse<T>{
+    data : T;
+}
+
+export interface GetMySpaceArchiveFoldersResponse{
+
+     folders : SpaceArchiveData[];
+
+}
 
 
 export interface PostMySpaceArchiveFolderRequest {
@@ -20,25 +31,27 @@ export const SpaceAPI = {
     /**
      * 마이스페이스의 아카이브 폴더 데이터를 조회하는 API입니다.
      */
-    async getMySpaceArchiveFoldersData(personalSpaceId : number | undefined): Promise<MySpaceArchiveDataResponse | null> {
+    async getMySpaceArchiveFoldersData(personalSpaceId : number | undefined) : Promise<GetMySpaceArchiveFoldersResponse | null> {
         if(!personalSpaceId){ return null;}
         
         console.log("API personalSpaceId ", personalSpaceId);
-        const res = await apiFetch<MySpaceArchiveDataResponse>(`/api/v1/spaces/${personalSpaceId}/folders`, {
+        const res = await apiFetch <ApiResponse<GetMySpaceArchiveFoldersResponse>>(`/api/v1/spaces/${personalSpaceId}/folders`, {
             method: "GET",
         });
 
-        console.log("API Res ",res.data.folders);
-        return res.data.folders;
+        return res.data;
     },
 
 
+    /*
+    * 마이스페이스의 아카이브 폴더를 추가하는 API입니다.
+    */
     async postMySpaceArchiveFolderData(personalSpaceId : number | undefined, folderData : PostMySpaceArchiveFolderRequest): 
     Promise<PostMySpaceArchiveFolderResponse | null> {
         if(!personalSpaceId){
             return null;
         }
-
+        console.log("폴더 데이터!! ", folderData)
         const res = await apiFetch<PostMySpaceArchiveFolderResponse>(`/api/v1/spaces/${personalSpaceId}/folders`, {
             method: "POST",
             body: JSON.stringify(folderData),
