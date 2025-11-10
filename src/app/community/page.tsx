ff8fa65246e19712c18fd7668332d0c78347ebc7
woghks2 <tbxkdls19@gmail.com>
2025-11-10 16:29:52 +0900

    refactor: 커뮤니티 게시글 공통 레이아웃 구성 [S13P31A208-231]
    - 커뮤니티 헤더, 커뮤니티 사이드바 인기글 섹션 레이아웃으로 구성

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,9 +1,7 @@
 // frontend/src/app/community/page.tsx
 
-import CommunityHeader from "@/components/layout/CommunityHeader";
 import CommunityTabs from "@/components/filter/CommunityTabs";
 import CommunityBoardSection from "@/components/section/CommunityBoardSection";
-import CommunityFloatingSection from "@/components/section/CommunityFloatingSection";
 import CommunityFooter from "@/components/layout/CommunityFooter";
 import { CommunityAPI } from "@/api/community";
 import { Suspense } from "react";
@@ -14,29 +12,16 @@ import { Suspense } from "react";
  * @returns {React.ReactNode}
  */
 export default async function CommunityPage() {
-
-  const { communityBoardList, popularPosts } = await CommunityAPI.getCommunityPageData();
+  const { communityBoardList } = await CommunityAPI.getCommunityBoardList();
 
   return (
-    <div className="min-h-screen bg-gray-50">
-      <CommunityHeader />
+    <>
       <Suspense fallback={<div>Loading...</div>}>
         <CommunityTabs />
       </Suspense>
 
-      <div className="max-w-7xl pl-8 pr-4 py-8 flex flex-row gap-6 relative">
-        
-        {/* 왼쪽: 필터 + 게시글 */}
-        <div className="flex-1 flex flex-col gap-6">
-          <CommunityBoardSection communityBoardList={communityBoardList} />
-          <CommunityFooter />
-        </div>
-
-        {/* 오른쪽: 플로팅 섹션 */}
-        <div className="hidden lg:block w-64 flex-shrink-0">
-          <CommunityFloatingSection popularPosts={popularPosts} />
-        </div>
-      </div>
-    </div>
+      <CommunityBoardSection communityBoardList={communityBoardList} />
+      <CommunityFooter />
+    </>
   );
 }
