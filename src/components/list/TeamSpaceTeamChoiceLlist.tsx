57e22a23cf2fb0a85f193c287c4f194949bb06fc
Jeong TaeSeung <mike415415@naver.com>
2025-11-11 15:58:24 +0900

    feat : team Space archive 페이지 들어가는 라우팅 방식 변경   [S13P31A208-253]

--- src/components/list/TeamSpaceTeamChoiceLlist.tsx
+++ src/components/list/TeamSpaceTeamChoiceLlist.tsx
@@ -9,15 +9,13 @@ export interface TeamSpaceTeamChoiceListProps {
 export default function TeamSpaceTeamChoiceList({teamSpaceTeamChoiceList} : TeamSpaceTeamChoiceListProps){
     // console.log("팀 스페이스 리스트 in list ", teamSpaceTeamChoiceList);
     return(
-          <div className="flex-1 px-5 grid grid-cols-4 auto-rows-max gap-x-7 gap-y-10">
+          <div className="px-5 grid grid-cols-4 auto-rows-max gap-x-7 gap-y-10">
                 {
                     teamSpaceTeamChoiceList?.map((item, index) => (
                         <TeamSpaceTeamChoiceItem
-
                             key={index}
                             spaceCoverUrl={item.spaceCoverUrl}
                             name={item.name}
-                            description={item.description}
                             participantCnt={item.participantCnt}
                             spaceId={item.spaceId}
                         />
