b668b6a7f306c34b1ccdb03ed41ef97e70519fa5
Jeong TaeSeung <mike415415@naver.com>
2025-11-11 19:41:35 +0900

    fix : 빌드 테스트 해결  [S13P31A208-253]

--- src/app/my-space/my-posts/page.tsx
+++ src/app/my-space/my-posts/page.tsx
@@ -27,7 +27,7 @@ export default async function MySpaceMyPostPage() {
       <MySpaceArchiveFilterSection buttonMode="write"/>
 
       <SpaceArchiveBoardList
-        mySpaceBoardList={mySpaceBoardList}
+        folderId ={0}
         />
 
     </div>
