0746d63bb5fa80f09ee5505c972e925c977a3ba9
Jeong TaeSeung <mike415415@naver.com>
2025-11-11 19:33:53 +0900

    feat : 마이 스페이스 아카이브 리스트 연결[S13P31A208-253]

--- src/components/list/SpaceArchiveBoardList.tsx
+++ src/components/list/SpaceArchiveBoardList.tsx
@@ -1,14 +1,67 @@
+'use client';
+import { useAuthStore } from "@/store/authStore";
 import SpaceArchiveBoardItem, { SpaceArchiveBoardItemProps } from "../item/SpaceArchiveBoardItem"
+import { useEffect, useState } from "react";
+import SpaceAPI, { ArticleData } from "@/api/space";
 
 
 
 export interface SpaceArchiveBoardListProps{
-    mySpaceBoardList : SpaceArchiveBoardItemProps[]
+    //  mySpaceBoardList : SpaceArchiveBoardItemProps[]
+    spaceId? : number;
+    folderId : number
 }
 
 
 
-export default function SpaceArchiveBoardList({mySpaceBoardList} : SpaceArchiveBoardListProps){
+export default function SpaceArchiveBoardList({
+    // mySpaceBoardList
+    spaceId,
+    folderId
+}: SpaceArchiveBoardListProps){
+        const [mySpaceBoardList, setMySpaceBoardList] =  useState<SpaceArchiveBoardItemProps[] | null>([]);
+        
+        const authStore = useAuthStore();
+
+        console.log("SpaceArchiveBoardList 폴더 아이디 ", spaceId);
+        //개인 스페이스의 경우 직접 authStore에서 가져와야 합니다.
+        if(spaceId === undefined){
+            
+            spaceId = authStore.user?.personalSpaceId;
+        }
+
+        useEffect(()=>{
+            if(!spaceId) return;
+
+            console.log("개인 스페이스 아이디 in useEffect ", spaceId);
+            // spaceId를 사용하여 필요한 작업 수행
+            const fetcher = async () =>
+                {
+                    console.log("??!!");
+                    const res = await SpaceAPI.getArchiveArticles(spaceId, folderId);
+                    const articles : ArticleData[] = res?.articles|| [];
+                    console.log("가져온 아카이브 글들 ", res?.articles);
+                    
+                    const boardList : SpaceArchiveBoardItemProps[]=[];
+                    for(const article of articles){
+                        boardList.push({
+                            id: article.articleId,
+                            category:article.type,
+                            title:article.title,
+                            tags: article.tags,
+                            image:article.fileUrl
+                        });
+                    }
+
+  
+
+                    setMySpaceBoardList(boardList);
+                    
+                    console.log("가져온 아카이브 글들2 ", boardList);
+                }
+
+            fetcher();
+        },[spaceId])
 
         return(
         <div className="flex flex-col px-4 gap-2">
