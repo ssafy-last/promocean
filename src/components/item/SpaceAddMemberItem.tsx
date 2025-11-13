e4fa9a03b7777d1e4d8031cd95789563865da316
Jeong TaeSeung <mike415415@naver.com>
2025-11-13 14:29:36 +0900

    feat : team manage modal ui 수정  [S13P31A208-253]

--- src/components/item/SpaceAddMemberItem.tsx
+++ src/components/item/SpaceAddMemberItem.tsx
@@ -1,28 +1,28 @@
+import { SpaceParticipants } from "@/api/space";
 
 
 export interface SpaceAddMemberItemProps {
-    name : string,
-    email : string
+    member : SpaceParticipants;
     isMinusButton? :boolean;
     handleMemberClick?: (memberName: string) => void;
 }
 
 
 
-export default function SpaceAddMemberItem({name, email, handleMemberClick, isMinusButton=true}: SpaceAddMemberItemProps){
+export default function SpaceAddMemberItem({member, handleMemberClick, isMinusButton=true}: SpaceAddMemberItemProps){
 
     return(
         <button 
         type="button"
         className ="flex flex-row w-full justify-between px-4 py-1 gap-3 items-center hover:bg-current/5 
         rounded-md"
-        onClick={()=>{handleMemberClick?.(name)}}
+        onClick={()=>{handleMemberClick?.(member.nickname)}}
         >
             <div className="flex gap-3 items-center justify-center-safe">
             <div className = "rounded-full bg-gray-200 border w-10 h-10"></div>
             <div className = "text-left">
-                <span className="font-medium text">{name}</span>
-                <p className="text-sm text-gray-400">{email}</p>
+                <span className="font-medium text">{member.nickname}</span>
+                <p className="text-sm text-gray-400">{member.email}</p>
             </div>
             </div>
 
