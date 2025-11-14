0b0e221b717981e83fd2d4447aebbdb24405e828
woghks <tbxkdls19@naver.com>
2025-11-14 13:08:09 +0900

    refactor: 커뮤니티, 대회 페이지 플로팅 레이아웃 수정 [S13P31A208-286]

--- src/app/community/layout.tsx
+++ src/app/community/layout.tsx
@@ -22,20 +22,23 @@ export default async function CommunityLayout({
   return (
     <div className="min-h-screen bg-gray-50">
       <CommunityHeader />
+
       <Suspense fallback={<div>Loading...</div>}>
         <CommunityTabs />
       </Suspense>
-      
-      <div className="max-w-7xl pl-8 pr-4 pt-8 pb-8 flex flex-row gap-6 relative">
-        {/* 왼쪽: 페이지 컨텐츠 */}
-        <div className="flex-1 flex flex-col gap-6">
+
+      <div className="flex flex-row gap-6 px-4 lg:px-8 pt-8 pb-8">
+        
+        {/* 왼쪽: 컨텐츠 (남는 공간 전부 사용) */}
+        <div className="flex-1 min-w-0 flex flex-col gap-6">
           {children}
         </div>
 
-        {/* 오른쪽: 플로팅 섹션 (인기글) */}
-        <div className="hidden lg:block w-64 flex-shrink-0">
+        {/* 오른쪽: 고정폭 플로팅 섹션 */}
+        <aside className="hidden lg:block w-64 flex-shrink-0 sticky top-24 self-start">
           <CommunityFloatingSection popularPosts={popularPosts} />
-        </div>
+        </aside>
+
       </div>
     </div>
   );
