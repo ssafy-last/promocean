07d3782e35521a0060e4c8f26b76ef49f1f480cf
전가배 <gabea52@naver.com>
2025-11-14 17:09:16 +0900

    hotfix:  redis 설정 변경으로 인한 RedisConfig수정

--- backend/src/main/java/com/ssafy/a208/global/redis/config/RedisConfig.java
+++ backend/src/main/java/com/ssafy/a208/global/redis/config/RedisConfig.java
@@ -3,27 +3,31 @@ package com.ssafy.a208.global.redis.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.util.List;
+
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String host;
+    @Value("${spring.data.redis.cluster.nodes}")
+    private List<String> clusterNodes;
 
-    @Value("${spring.data.redis.port}")
-    private int port;
-
+    //클러스트 모드로 변경..!
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisClusterConfiguration clusterConfig = new RedisClusterConfiguration(clusterNodes);
+        return new LettuceConnectionFactory(clusterConfig);
     }
 
+    /**
+     * RedisTemplate 설정
+     */
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
@@ -31,12 +35,10 @@ public class RedisConfig {
 
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
-
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         template.afterPropertiesSet();
         return template;
     }
-
 }
