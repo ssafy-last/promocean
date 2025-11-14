44dde416a06a30097e37f0f6dccd95de41cfc5c2
woghks <tbxkdls19@naver.com>
2025-11-14 11:06:20 +0900

    fix: 상세 게시물 페이지에서 layout으로 분리한 헤더가 나오는 에러 수정 [S13P31A208-286]

--- src/app/contest/[contestId]/page.tsx
+++ src/app/contest/[contestId]/page.tsx
@@ -1,8 +1,6 @@
 // frontend/src/app/contest/[contestId]/page.tsx
 
-import ContestHeader from "@/components/layout/ContestHeader";
 import ContestPostSection from "@/components/section/ContestPostSection";
-// import ContestInfoSection from "@/components/section/ContestInfoSection";
 import { ContestAPI } from "@/api/contest"; 
 
 interface ContestPostPageProps {
@@ -11,7 +9,7 @@ interface ContestPostPageProps {
 
 /**
  * ContestPostPage component
- * @description 대회 상세 게시글 페이지 (mock → 실제 API 전환 예정)
+ * @description 대회 상세 게시글 페이지
  * @returns {React.ReactNode}
  */
 export default async function ContestPostPage({ params }: ContestPostPageProps) {
@@ -20,39 +18,21 @@ export default async function ContestPostPage({ params }: ContestPostPageProps) 
 
   const {
     contestPostData,
-    // leaderboardList,
-    // contestInfoData,
-    // contestInfoTitles,
     contestNoticeList,
     contestSubmissionList,
-    // contestMySubmissionList,
   } = await ContestAPI.getContestPostPageData(contestId);
 
   return (
     <div className="min-h-screen bg-gray-50">
-      
-      {/* TODO : /contest 상위 layout.tsx로 이동 고려 */}
-      <ContestHeader />
-
       <div className="max-w-7xl pl-8 pr-4 py-8 flex flex-row gap-6 relative">
         {/* 왼쪽: 게시글 섹션 */}
         <div className="flex-1 flex flex-col gap-6 bg-white rounded-lg shadow-md">
           <ContestPostSection
             contestPostData={contestPostData}
-            // leaderboardList={leaderboardList} TODO : 삭제
             ContestNoticeList={contestNoticeList}
             contestSubmissionList={contestSubmissionList}
-            // contestMySubmissionList={contestMySubmissionList}
           />
         </div>
-
-        {/* 오른쪽: 플로팅 정보 섹션 */}
-        {/* <div className="hidden lg:block w-64 flex-shrink-0">
-          <ContestInfoSection
-            titles={contestInfoTitles}
-            items={contestInfoData}
-          />
-        </div> */}
       </div>
     </div>
   );
