fc7e3917c1699ed81401d1b943abb9b132db6ede
gabea52 <gabea52@gmail.com>
2025-11-14 09:51:27 +0900

    feat: Kafka 설정 및 Topic 구성, dto 추가 [S13P31A208-295] +application-local.yml 수정

---
+++ backend/src/main/java/com/ssafy/a208/domain/board/dto/TrendingPostRes.java
@@ -0,0 +1,17 @@
+package com.ssafy.a208.domain.board.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+@Schema(description = "실시간 인기글 조회 응답")
+public record TrendingPostRes(
+
+        @Schema(description = "인기글 리스트")
+        List<TrendingPostItemDto> posts,
+
+        @Schema(description = "조회된 게시글 개수", example = "10")
+        Integer itemCnt
+) {}
