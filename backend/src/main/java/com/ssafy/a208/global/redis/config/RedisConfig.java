908f3c93d943de957ab6f2bbd37786fdd8b519e0
전가배 <gabea52@gmail.com>
2025-11-14 18:30:10 +0900

    Edit RedisConfig.java

--- backend/src/main/java/com/ssafy/a208/global/redis/config/RedisConfig.java
+++ backend/src/main/java/com/ssafy/a208/global/redis/config/RedisConfig.java
@@ -3,25 +3,38 @@ package com.ssafy.a208.global.redis.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import io.lettuce.core.ReadFrom;
+
+import java.time.Duration;
+import java.util.List;
+
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String host;
-
-    @Value("${spring.data.redis.port}")
-    private int port;
+    @Value("${spring.data.redis.cluster.nodes}")
+    private List<String> clusterNodes;
 
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+    public RedisConnectionFactory redisConnectionFactory() {
+
+        RedisClusterConfiguration clusterConfig = new RedisClusterConfiguration(clusterNodes);
+        clusterConfig.setMaxRedirects(3);
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .useSsl()
+                .commandTimeout(Duration.ofSeconds(5))
+                .readFrom(ReadFrom.REPLICA_PREFERRED)
+                .build();
+
+        return new LettuceConnectionFactory(clusterConfig, clientConfig);
     }
 
     @Bean
@@ -31,12 +44,10 @@ public class RedisConfig {
 
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
-
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         template.afterPropertiesSet();
         return template;
     }
-
 }
