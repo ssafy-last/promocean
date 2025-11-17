87a292f89634d09911b0cd648378b8604f5a8400
gabea52 <gabea52@naver.com>
2025-11-17 11:14:14 +0900

    feat: 인기글 초기 데이터 동기화 및 스케줄러 구현 [S13P31A208-301]

--- backend/src/main/java/com/ssafy/a208/domain/board/repository/PostRepository.java
+++ backend/src/main/java/com/ssafy/a208/domain/board/repository/PostRepository.java
@@ -4,9 +4,13 @@ import com.ssafy.a208.domain.board.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
     Optional<Post> findByIdAndDeletedAtIsNull(Long postId);
+
+    List<Post> findAllByCreatedAtAfterAndDeletedAtIsNull(LocalDateTime createdAt);
 }
