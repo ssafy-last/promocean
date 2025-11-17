1337fdafe9a1ad5bd2d8bcfce43a8cd4b9feb8c4
gabea52 <gabea52@gmail.com>
2025-11-17 11:14:14 +0900

    feat: 인기글 초기 데이터 동기화 및 스케줄러 구현 [S13P31A208-301]

--- backend/src/main/java/com/ssafy/a208/PromoceanApplication.java
+++ backend/src/main/java/com/ssafy/a208/PromoceanApplication.java
@@ -3,9 +3,11 @@ package com.ssafy.a208;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableScheduling
 public class PromoceanApplication {
 
     public static void main(String[] args) {
