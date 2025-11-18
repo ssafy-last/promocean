a2f29ccbe9312f57e4bcedae35e04e12f7645b09
Jeong TaeSeung <mike415415@naver.com>
2025-11-18 10:29:17 +0900

    feat : 아카이브 폴더 ui 미세 수정 [S13P31A208-254 ]

--- src/app/my-space/scrap/page.tsx
+++ src/app/my-space/scrap/page.tsx
@@ -1,12 +1,15 @@
 import { SpaceBoardHeader } from "@/components/layout/SpaceBoardHeader";
 import SpaceScrapBoardList from "@/components/list/SpaceScrapBoardList";
+import { Suspense } from "react";
 
 // frontend/src/app/my-space/scrap/page.tsx
 export default function MySpaceScrapPage() {
   return (
     <div className="min-h-screen bg-gray-50">
       <SpaceBoardHeader title="스크랩" description="스크랩한 글을 모아볼 수 있습니다." />
-      <SpaceScrapBoardList />
+      <Suspense>      <SpaceScrapBoardList />
+        <SpaceScrapBoardList />
+      </Suspense>`
     </div>
   );
 }
