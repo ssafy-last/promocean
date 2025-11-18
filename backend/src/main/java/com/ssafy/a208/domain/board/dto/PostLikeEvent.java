62784c31729f85128a16c9bd4a2acc6a5c6da62f
전가배 <gabea52@naver.com>
2025-11-18 16:06:29 +0900

    feature: 실시간 인기글 구현 + ES 안된 부분 업데이트 [S13P31A208-246]

---
+++ backend/src/main/java/com/ssafy/a208/domain/board/dto/PostLikeEvent.java
@@ -0,0 +1,19 @@
+package com.ssafy.a208.domain.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostLikeEvent {
+    private String eventId;
+    private Long postId;
+    private Long userId;
+    private LocalDateTime timestamp;
+}
