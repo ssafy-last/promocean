83afaf751121d0a4c9f564688057f9baf2095011
Jeong TaeSeung <mike415415@naver.com>
2025-11-19 18:36:51 +0900

    feat: 메인 페이지 재디자인 작업 2 (대회 리스트 섹션 추가) [S13P31A208-302]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import HeroSection from "@/components/section/HeroSection";
 import PostCardSection from "@/components/section/PostCardSection";
 import { PostAPI } from "@/api/community";
+import { ContestAPI } from "@/api/contest/contest";
+import OngoingContestSection from "@/components/section/OngoingContestSection";
 
 // ISR: 60초마다 재생성
 export const revalidate = 60;
@@ -16,14 +18,22 @@ export default async function Home() {
 
   // TODO : 카테고리 렌더링 할거면 요청해야함
   const { popularPosts } = await PostAPI.getPopular();
-
+  const {contestCardList } = await ContestAPI.getList();
+  console.log("context ",contestCardList);
   return (
     <>
       <HeroSection />
+      <div className = "flex flex-col gap-4 pt-4">
       <PostCardSection
         postSectionTitle="인기 프롬프트"
         postCardList={popularPosts}
       />
+      
+      <OngoingContestSection contests={contestCardList} />
+      </div>
+      <footer className="mt-20 mb-10 text-center text-sm text-gray-400">
+        &copy; 2024 Your Company. All rights reserved.
+      </footer>  
     </>
   );
 }
