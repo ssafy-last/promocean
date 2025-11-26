9486b532f8d18841499d32f236340cfa5b6d079a
Jeong TaeSeung <mike415415@naver.com>
2025-11-26 09:37:55 +0900

    feat : 로그인 디자인 변경 완료  [S13P31A208-340]

--- src/components/filter/LoginTabs.tsx
+++ src/components/filter/LoginTabs.tsx
@@ -31,7 +31,7 @@ export default function LoginTabs({ activeTab, onTabChange }: LoginTabsProps) {
         회원가입
       </button>
 
-      <div className ={`absolute inset-0 border-b-3 border-b-primary w-15 py-3 transition-[translate] duration-300 ${
+      <div className ={`absolute inset-0 border-b-3 border-b-primary w-15 py-3 transition-[translate] duration-200 ease-out ${
           activeTab === 'signup'
             ? 'text-primary border-b-2 border-primary translate-x-[120%]'
             : 'text-text/60 hover:text-primary translate-x-[-15%]'
